c8579c1b6aa4d10231a45f94d07cfa6cb6b01c35
LuisMC20 <140764479+LuisMC20@users.noreply.github.com>
2024-06-25 18:00:26 -0400

    Update apollo.js

--- config/apollo.js
+++ config/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`, // Usar la variable de entorno aquí
+  uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`, // Usar la variable de entorno aquí y añadir /graphql
   credentials: 'include', // Incluir cookies y credenciales
 });
 
