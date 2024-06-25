8f86466f2e16d2e0dbd9a8c328102b5ef32c0756
LuisMC20 <140764479+LuisMC20@users.noreply.github.com>
2024-06-25 19:28:35 -0400

    Update apollo.js

--- config/apollo.js
+++ config/apollo.js
@@ -8,6 +8,7 @@ const httpLink = createHttpLink({
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
+  console.log('Token from localStorage:', token);
   return {
     headers: {
       ...headers,
