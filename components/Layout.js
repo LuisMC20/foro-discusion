cdd5c511cf05c86de2ee0d1c4754d81d59a0f3d1
LuisMC20 <140764479+LuisMC20@users.noreply.github.com>
2024-06-26 15:25:23 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -21,10 +21,10 @@ const Layout = ({ children }) => {
   const router = useRouter();
   
   const backgroundImageUrl = "/images/layoutbg3.jpg"; // Reemplaza con la ruta de tu imagen de fondo
-  const { data, loading, error} = useQuery(OBTENER_ANUNCIOS);
+  const { data, loading, error } = useQuery(OBTENER_ANUNCIOS);
 
-   if (loading) return <p>Cargando anuncios de bd...</p>;
-   if (error) return <p>Error al cargar anuncios de RENDER: {error.message}</p>;
+  if (loading) return <p>Cargando anuncios de bd...</p>;
+  if (error) return <p>Error al cargar anuncios de RENDER: {error.message}</p>;
 
   return (
     <>
