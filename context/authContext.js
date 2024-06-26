2a21ba0e20deb99af87a9b44e9d29b12414db96e
LuisMC20 <140764479+LuisMC20@users.noreply.github.com>
2024-06-26 15:37:43 -0400

    Update authContext.js

--- context/authContext.js
+++ context/authContext.js
@@ -7,12 +7,11 @@ const AuthContext = createContext();
 
 const createApolloClient = (token) => {
   const httpLink = createHttpLink({
-    uri: `https://foro-discusiones-backend.onrender.com/graphql`, // Usar la variable de entorno aquí y añadir /graphql
-    credentials: 'include', // Asegúrate de incluir credenciales para las cookies y encabezados de autenticación
+    uri: `https://foro-discusiones-backend.onrender.com/graphql`,
+    credentials: 'include',
   });
 
   const authLink = setContext((_, { headers }) => {
-    console.log('Token for Apollo Client:', token);
     return {
       headers: {
         ...headers,
@@ -33,19 +32,16 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null;
-    console.log('Token from localStorage in useEffect:', token);
 
     if (token) {
       const client = createApolloClient(token);
       client
         .query({ query: OBTENER_USUARIO })
         .then(({ data }) => {
-          console.log('Usuario obtenido:', data.obtenerUsuario);
           setUser(data.obtenerUsuario);
           setIsAuthenticated(true);
         })
         .catch((error) => {
-          console.error('Error al obtener el usuario:', error);
           setIsAuthenticated(false);
           setUser(null);
         });
@@ -54,14 +50,12 @@ export const AuthProvider = ({ children }) => {
 
   const login = (token, userData) => {
     localStorage.setItem('token', token);
-    console.log('Token set in localStorage:', token);
     setUser(userData);
     setIsAuthenticated(true);
   };
 
   const logout = () => {
     localStorage.removeItem('token');
-    console.log('Token removed from localStorage');
     setUser(null);
     setIsAuthenticated(false);
   };
